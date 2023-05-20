c3e747481d207317186a692ebd5711d323a33206
AlexandrIvanov <alexandr.1996@list.ru>
2023-05-20 14:26:05 +0400

    Add Controller

--- src/pages/auth-page/auth-form/auth-form.tsx
+++ src/pages/auth-page/auth-form/auth-form.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button, Checkbox, FormControlLabel, Link, TextField, Typography} from '@mui/material';
-import style from './auth-form.module.css'
-import {useForm} from 'react-hook-form';
+import style from './auth-form.module.css';
+import {Controller, SubmitHandler, useForm} from 'react-hook-form';
 
 type FormDataType = {
     email: string
@@ -11,11 +11,14 @@ type FormDataType = {
 
 export const AuthForm = () => {
 
-    const {register, handleSubmit} = useForm<FormDataType>()
+    const {register, handleSubmit, control, formState: {errors}} = useForm<FormDataType>();
 
-    const onSubmit = (data: FormDataType) => {
-        console.log(data)
-    }
+    const onSubmit: SubmitHandler<FormDataType> = (data) => {
+        console.log(data);
+    };
+
+    console.log(!!errors);
+
 
     return (
         <div className={style.authForm}>
@@ -25,34 +28,60 @@ export const AuthForm = () => {
 
             <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
 
+                <Controller
+                    control={control}
+                    name="email"
+                    render={({field}) => <TextField
+                        label={'Email'}
+                        variant="outlined"
+                        margin={'normal'}
+                        fullWidth
+                        {...register('email', {required: 'This field is required'})}
+                        value={field.value || ''}
+                        onChange={(e) => field.onChange(e)}
+                        helperText={errors.email?.message}
+                        error={!!errors.email}
+                    />
+                    }
+                />
 
+                <Controller
+                    control={control}
+                    name="password"
+                    render={({field}) => <TextField
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        margin={'normal'}
+                        fullWidth
+                        {...register('password', {required: 'This field is required',  minLength: {value: 4, message: 'Min length is 4'}})}
+                        value={field.value || ''}
+                        onChange={(e) => field.onChange(e)}
+                        helperText={errors.password?.message}
+                        error={!!errors.password}
+                    />
+                    }
+                />
 
-                <TextField
-                    label="Email"
-                    variant="outlined"
-                    {...register('email')}
-                    margin={'normal'}
-                    fullWidth
+                <Controller
+                    control={control}
+                    name="rememberMe"
+                    render={({field}) => (
+                        <FormControlLabel
+                            control={<Checkbox/>}
+                            label="Remember me"
+                            value={field.value}
+                            onChange={(e) => field.onChange(e)}
+                        />
+                    )}
                 />
-                <TextField
-                    label="Password"
-                    type={'password'}
-                    variant="outlined"
-                    {...register('password')}
-                    margin={'normal'}
-                    fullWidth
-                />
-                <FormControlLabel
-                    control={<Checkbox defaultChecked/>}
-                    label="Remember me"
-                    {...register('rememberMe')}
-                />
-                <Button variant="contained" type={'submit'} sx={{marginBottom: '10px'}} fullWidth>Login</Button>
+
+                <Button variant="contained" type={'submit'} sx={{marginBottom: '10px'}} fullWidth disabled={!!errors.email && !!errors.password}>Login</Button>
             </form>
 
             <Typography variant="subtitle1">
                 New User? <Link href="#">Register</Link>
             </Typography>
         </div>
-    )
-}
+    );
+};
